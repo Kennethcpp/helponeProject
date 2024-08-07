ffae92d7244a484f1e23f22eca61903a45635716
kennethcpp <user.kenzydkenzy@gmail.com>
2024-08-07 05:48:09 +0100

    updated

--- client/signUp.js
+++ client/signUp.js
@@ -11,14 +11,27 @@ window.addEventListener("DOMContentLoaded", async () => {
         const phoneNumber = document.getElementById("phoneNumber").value;
   
         const user = await fetch("https://helponeproject.onrender.com/register", {
-          method: "post",
+          method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(obj),
+          body: JSON.stringify({
+        
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber,
+
+          }),
         })
           .then((res) => res.json())
           .catch((error) => console.log(error));
+          window.localStorage.setItem("User_firstname", firstname);
+          window.localStorage.setItem("User_lastname", lastname);
+          window.localStorage.setItem("User_email", email);
+          window.localStorage.setItem("User_phoneNumber", phoneNumber);
+          window.localStorage.setItem("User_id", User._id);
 
         window.location.href = "/account.html";
       });
